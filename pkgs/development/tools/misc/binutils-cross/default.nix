e69b0491b11c3b3b120619d134d6c6c428010001
Armijn Hemel <armijn@gpl-violations.org>
2007-03-13 09:31:07 +0000

    new binutils, new kernel headers
    svn path=/nixpkgs/branches/cross-compilation/; revision=8274

--- pkgs/development/tools/misc/binutils-cross/default.nix
+++ pkgs/development/tools/misc/binutils-cross/default.nix
@@ -1,11 +1,14 @@
 {stdenv, fetchurl, noSysDirs, cross}:
 
 stdenv.mkDerivation {
-  name = "binutils-2.16.1";
+  #name = "binutils-2.16.1";
+  name = "binutils-2.17";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/binutils-2.16.1.tar.bz2;
-    md5 = "6a9d529efb285071dad10e1f3d2b2967";
+    #url = http://nix.cs.uu.nl/dist/tarballs/binutils-2.16.1.tar.bz2;
+    #md5 = "6a9d529efb285071dad10e1f3d2b2967";
+    url = http://nix.cs.uu.nl/dist/tarballs/binutils-2.17.tar.bz2;
+    md5 = "e26e2e06b6e4bf3acf1dc8688a94c0d1";
   };
   inherit noSysDirs;
   #configureFlags = if cross=="arm-linux" then "--target=arm-linux" else if cross=="mips-linux" then "--target=mips-linux" else if cross=="sparc-linux" then "--target=sparc-linux"; 
--- pkgs/os-specific/linux/kernel-headers-cross/default.nix
+++ pkgs/os-specific/linux/kernel-headers-cross/default.nix
@@ -3,11 +3,11 @@
 assert stdenv.system == "i686-linux";
 
 stdenv.mkDerivation {
-  name = "linux-headers-2.6.14.5";
+  name = "linux-headers-2.6.19.7";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/linux-2.6.14.5.tar.bz2;
-    md5 = "9f057e3bd31c50dc48553def01bc8037";
+    url = ftp://ftp.nluug.nl/pub/os/Linux/system/kernel/v2.6/linux-2.6.19.7.tar.bz2;
+    sha256 = "1ygspwl019d4aypn3kd0pp9r3650aslm6zpfdspp1v575l6gcmy8";
   };
   inherit cross;
 }
