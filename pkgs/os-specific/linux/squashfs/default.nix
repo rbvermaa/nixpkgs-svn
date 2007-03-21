da92951201dfe9990c0ae79ae3ad8bee74fed099
Armijn Hemel <armijn@gpl-violations.org>
2007-03-21 00:29:20 +0000

    newer squashfs version. squashfs is used on a lot of embedded Linux devices. This needs to be merged back into the regular nixpkgs.
    svn path=/nixpkgs/branches/cross-compilation/; revision=8392

--- pkgs/os-specific/linux/squashfs/default.nix
+++ pkgs/os-specific/linux/squashfs/default.nix
@@ -1,11 +1,11 @@
 {stdenv, fetchurl, zlib}:
 
 stdenv.mkDerivation {
-  name = "squashfs-3.1-r2";
+  name = "squashfs-3.2-r2";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://nix.cs.uu.nl/dist/tarballs/squashfs3.1-r2.tar.gz;
-    md5 = "c252e5286b142afa54ca49829c51a33f";
+    url = http://switch.dl.sourceforge.net/sourceforge/squashfs/squashfs3.2-r2.tar.gz;
+    sha256 = "19s01d2wsqy4d3f5wjwkywy8qz0zj8sc0bxk1339k06pb1fld0l4";
   };
   buildInputs = [zlib];
 }
