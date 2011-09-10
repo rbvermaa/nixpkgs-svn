1345b068386176ecccdd93a530c230429936b5c5
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-09-10 13:02:24 +0000

    * fftw updated to 3.3, and follow the coding guidelines.
    svn path=/nixpkgs/branches/syscall-tracing/; revision=29153

--- pkgs/development/libraries/fftw/default.nix
+++ pkgs/development/libraries/fftw/default.nix
@@ -1,31 +1,29 @@
-{fetchurl, stdenv, builderDefs, stringsWithDeps, singlePrecision ? false}:
-let localDefs = builderDefs.passthru.function { 
-  src = 
-    fetchurl {
-      url = ftp://ftp.fftw.org/pub/fftw/fftw-3.2.2.tar.gz;
-      sha256 = "13vnglardq413q2518zi4a8pam3znydrz28m9w09kss9xrjsx9va";
-    };
-  buildInputs = [];
-  configureFlags = ["--enable-shared" "--enable-openmp"]
-                        # some distros seem to be shipping both versions within the same package?
-                        # why does --enable-float still result in ..3f.so instead of ..3.so?
-                   ++ (if singlePrecision then [ "--enable-single" ] else [ ])
-                        # I think all i686 has sse
-                   ++ (if (stdenv.isi686 || stdenv.isx86_64) && singlePrecision then [ "--enable-sse" ] else [ ])
-                        # I think all x86_64 has sse2
-                   ++ (if stdenv.isx86_64 && ! singlePrecision then [ "--enable-sse2" ] else [ ]);
-                
+{ fetchurl, stdenv, singlePrecision ? false }:
+
+with stdenv.lib;
+
+let version = "3.3"; in
+
+stdenv.mkDerivation rec {
+  name = "fftw-${version}" + (if singlePrecision then "-single" else "-double");
+
+  src = fetchurl {
+    url = "ftp://ftp.fftw.org/pub/fftw/fftw-${version}.tar.gz";
+    sha256 = "1skakcijq5rds6mmj7jffqk5i4fw7p81k4z1iikkx4qk3999hnnj";
   };
-in with localDefs;
-stdenv.mkDerivation {
-  name = "fftw-3.2.2" + ( if singlePrecision then "-single" else "-double" );
-  builder = writeScript "fftw-3.2.1-builder"
-    (textClosure localDefs [doConfigure doMakeInstall doForceShare]);
+  
+  configureFlags =
+    [ "--enable-shared" "--enable-openmp" ]
+    # some distros seem to be shipping both versions within the same package?
+    # why does --enable-float still result in ..3f.so instead of ..3.so?
+    ++ optional singlePrecision "--enable-single"
+    # I think all i686 has sse
+    ++ optional ((stdenv.isi686 || stdenv.isx86_64) && singlePrecision) "--enable-sse"
+    # I think all x86_64 has sse2
+    ++ optional (stdenv.isx86_64 && !singlePrecision) "--enable-sse2";
+
   meta = {
-    description = "Fastest Fourier Transform in the West library";
-  };
-  passthru = {
-    # Allow instantiating "-A fftw.src"
-    inherit src;
+    homepage = http://www.fftw.org/;
+    description = "A C subroutine library for computing the discrete Fourier transform";
   };
 }
